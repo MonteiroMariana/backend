805561b43a371019ad7104c1644f869178d712cb
MonteiroMariana <marianaleitemonteiro@gmail.com>
2025-07-10 12:05:45 -0300

    Salvar minhas mudanças antes do rebase

--- contatos-app/src/app/contatos/contatos.service.ts
+++ contatos-app/src/app/contatos/contatos.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Contato } from './contato.model';
+import { environment } from '../../environments/environment'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContatosService {
-  private api = 'https://javacontato.duckdns.org/contatos';
+  private api = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ contatos-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://javacontato.duckdns.org/contatos'
+};
